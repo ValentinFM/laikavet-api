2a28c680fa5584fc7be4f26f18c088cde9122aa4
MarcoDev <marcobarzoladev@gmail.com>
2025-12-02 16:39:47 -0300

    Agregando modificar y eliminar

--- config/db.js
+++ config/db.js
@@ -2,12 +2,11 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    // viene del archivo .env
     await mongoose.connect(process.env.MONGODB_URI);
     console.log('MongoDB conectada exitosamente (LaikaVet) ✅');
   } catch (error) {
     console.error(`Error de conexión: ${error.message}`);
-    process.exit(1); // Detiene la app si no se puede conectar
+    process.exit(1); // Esto debe estar activo ahora
   }
 };
 
